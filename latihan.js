5cc5d830aa98ff7259e0a3c1e27eb67e96c6e29a
unknown <agiariyan@gmail.com>
2021-01-17 09:03:27 +0700

    first commit

---
+++ latihan.js
@@ -0,0 +1,11 @@
+
+function generateSoal() {
+    var table = document.getElementById("txtMatrix");
+    var row = table.insertRow(0);
+    var matrix1 = newRow.insertCell(0);
+    var matrix2 = newRow.insertCell(1);
+    matrix1.innerHTML = " ";
+    matrix2.innerHTML = " ";
+  }
+    console.log(matrix1);
+    console.log(matrix2);
